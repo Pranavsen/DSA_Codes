21771b42d073761c46b5a274fd9b4ff00b9ad267
Pranav Sen <pranav.sen2019@vitbhopal.ac.in>
2023-11-18 00:22:32 +0530

    DSA

--- DSA_In_Java/BinarySearchTree/KthLargestBST.java
+++ DSA_In_Java/BinarySearchTree/KthLargestBST.java
@@ -12,43 +12,37 @@ public class KthLargestBST {
     }
 
     public static int ans;
+    public static int count;
 
-    public static void solve(Node root, int k, int count) {
+    public static void solve(Node root, int k) {
         if (root == null) {
             return;
         }
-        solve(root.right, k, count);
+        solve(root.right, k);
         if (k == count) {
             ans = root.data;
-            return;
+            count++;
         } else {
             count++;
         }
-        solve(root.left, k, count);
+        solve(root.left, k);
     }
 
     public static int kthLargest(Node root, int k) {
-        int count = 1;
+        count = 1;
         ans = -1;
-        solve(root, k, count);
+        solve(root, k);
         return ans;
     }
-    // public static void inorder(Node root){
-    //     if(root==null) return;
-    //     inorder(root.left);
-    //     System.out.println(root.data+" ");
-    //     inorder(root.right);
-    // }
 
     public static void main(String[] args) {
         Node root = new Node(8);
-        root.left = new Node(4);
+        root.left = new Node(6);
         root.right = new Node(11);
         root.left.left = new Node(2);
-        root.left.right = new Node(6);
-        root.right.left = new Node(10);       
-         root.right.right = new Node(12);
-
+        root.left.right = new Node(7);
+        root.right.left = new Node(9);
+        root.right.right = new Node(12);
 
         int k = 2;
         int sol = kthLargest(root, k);
--- DSA_In_Java/BinarySearchTree/KthSmallest.java
+++ DSA_In_Java/BinarySearchTree/KthSmallest.java
@@ -12,39 +12,39 @@ public class KthSmallest {
     }
 
     public int ans;
-
-    public void solve(Node root, int k, int count) {
+public int count;
+    public void solve(Node root, int k) {
         if (root == null) {
             return;
         }
-        solve(root.left, k, count);
+        solve(root.left, k);
         if (k == count) {
             ans = root.data;
-            return;
+            count++;
         } else {
             count++;
         }
-        solve(root.right, k, count);
+        solve(root.right, k);
     }
 
     public int kthSmall(Node root, int k) {
-        int count = 1;
+        count = 1;
         ans = -1;
-        solve(root, k, count);
+        solve(root, k);
         return ans;
     }
 
     public static void main(String[] args) {
         KthSmallest kthSmallest = new KthSmallest();
         Node root = new Node(8);
-        root.left = new Node(4);
+        root.left = new Node(6);
         root.right = new Node(11);
         root.left.left = new Node(2);
-        root.left.right = new Node(6);
-        root.right.left = new Node(10);
+        root.left.right = new Node(7);
+        root.right.left = new Node(9);
         root.right.right = new Node(12);
 
-        int k = 3;
+        int k = 2;
         int sol = kthSmallest.kthSmall(root, k);
         System.out.println("Kth Smallest element is : " + sol);
 
